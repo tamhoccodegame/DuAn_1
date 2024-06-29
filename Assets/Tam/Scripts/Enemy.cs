e35d51b224ee95062dfcb0f8d2e60bb80eba8f5c
Tam Doan <tamdoan246@gmail.com>
2024-06-29 18:12:15 +0700

    Update Enemy

--- Assets/Tam/Scripts/Enemy.cs
+++ Assets/Tam/Scripts/Enemy.cs
@@ -4,33 +4,132 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-	protected Transform player;
+    protected Transform player;
 
     protected float maxHealth;
     protected float currentHealth;
-	protected float speed;
+    protected float patrolSpeed;
+    protected float chaseSpeed;
+    protected float attackRange;
+    protected float chaseRange;
+    protected Transform[] patrolPoints;
+    private int currentPatrolIndex;
 
-	protected Rigidbody2D rb;
+    protected Rigidbody2D rb;
     protected Animator animator;
-     
+
+    protected Vector3 direction;
+
+    public enum State
+    {
+        Patrol,
+        Attack,
+        Chase,
+    }
+
+    public State currentState;
+
     // Start is called before the first frame update
     public virtual void Start()
     {
-        
+        currentState = State.Patrol;
+        currentPatrolIndex = 0;
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame  
     void Update()
     {
- 
+        switch (currentState)
+        {
+            case State.Patrol:
+                Patrol();
+                break;
+            case State.Attack:
+                Attack();
+                break;
+            case State.Chase:
+                Chase();
+                break;
+        }
     }
 
-    protected void Attack()
+    private void ChangeState(State newState)
     {
-        animator.SetTrigger("isAttack"); 
+        currentState = newState;
     }
 
-    public void TakeDamage(float damage)
+
+    private void Patrol()
+    {
+        if (Mathf.Abs(player.position.x - transform.position.x) <= chaseRange)
+        {
+            ChangeState(State.Chase);
+        }
+
+        Transform targetPoint = patrolPoints[currentPatrolIndex];
+        direction = new Vector3(targetPoint.position.x - transform.position.x, 0, 0);
+        direction.Normalize();
+        LookAtDirection(direction);
+
+        rb.velocity = new Vector2(direction.x * patrolSpeed, rb.velocity.y);
+
+        if (Vector2.Distance(transform.position, targetPoint.position) <= 0.5f)
+        {
+            rb.velocity = Vector2.zero;
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+        }
+
+    }
+
+    private void LookAtDirection(Vector3 direction)
+    {
+        transform.localScale = new Vector3(direction.x * Mathf.Abs(transform.localScale.x),
+                                          transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Chase()
+    {
+        direction = new Vector3(player.position.x - transform.position.x, 0, 0);
+        direction.Normalize();
+
+        LookAtDirection(direction);
+
+        rb.velocity = new Vector2(direction.x * chaseSpeed, rb.velocity.y);
+
+        if (Mathf.Abs(player.position.x - transform.position.x) <= attackRange)
+        {
+            ChangeState(State.Attack);
+        }
+        if (Mathf.Abs(player.position.x - transform.position.x) > chaseRange)
+        {
+            ChangeState(State.Patrol);
+        }
+        //Debug.Log("Direction: " + direction + " | Velocity: " + rb.velocity);
+    }
+
+    private void Attack()
+    {
+        rb.velocity = Vector2.zero;
+        animator.SetTrigger("isAttack");
+        if (Mathf.Abs(player.position.x - transform.position.x) > attackRange)
+        {
+            StartCoroutine(AttackDelay());           
+        }
+    }
+
+    private IEnumerator AttackDelay()
+    {
+		animator.ResetTrigger("isAttack");
+		yield return new WaitForSeconds(2f);
+        
+		ChangeState(State.Chase);
+	}
+
+
+	public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         if (currentHealth < 0)
@@ -42,6 +141,7 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         this.enabled = false;
+        animator.SetBool("isDead", true);
     }
 
 
--- Assets/Tam/Scripts/normalEnemy.cs
+++ Assets/Tam/Scripts/normalEnemy.cs
@@ -5,33 +5,28 @@ using UnityEngine;
 public class normalEnemies : Enemy
 {
 	[SerializeField] private float _maxHealth;
-	[SerializeField] private float _speed = 2f;
+	[SerializeField] private float _patrolSpeed = 2f;
+	[SerializeField] private float _chaseSpeed = 5f;
+	[SerializeField] private float _attackRange = 2.5f;
+	[SerializeField] private float _chaseRange = 15f;
+	[SerializeField] private Transform[] _patrolPoints;
 
-	private Transform player;
-	private Rigidbody2D rb;
 
 	// Start is called before the first frame update
 	public override void Start()
     {
-		player = GameObject.Find("Player").GetComponent<Transform>();
-		rb = GetComponent<Rigidbody2D>();
+		base.Start();
+		patrolSpeed = _patrolSpeed;
+		chaseSpeed = _chaseSpeed;
+		attackRange = _attackRange;
+		chaseRange = _chaseRange;
         maxHealth = _maxHealth;
 		currentHealth = _maxHealth;
+		patrolPoints = _patrolPoints;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Chase();
-    }
-
-	private void Chase()
-	{
-		Vector3 direction = new Vector3(player.position.x - transform.position.x, 0, 0);
-		direction.Normalize();
-		rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
-
-	}
-
+	
+	
 	
 }
