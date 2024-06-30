84718ad25bfe667dd1c3e3305b62018dc01cf7e0
Tam Doan <tamdoan246@gmail.com>
2024-06-30 22:03:32 +0700

    fix healthBar and adjus Enemy

---
+++ Assets/HitBox.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+	private int damage;
+	int playerDamage;
+	int enemyDamage;
+
+	private void Start()
+	{
+		//playerDamage = 
+		enemyDamage = GetComponentInParent<Enemy>().GetDamage();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Enemy enemyHit = collision.gameObject.GetComponent<Enemy>();
+		Player_Health playerHit = collision.gameObject.GetComponent<Player_Health>();
+		Debug.Log(playerHit);
+		if (playerHit)
+		{
+			playerHit.TakeDamage(enemyDamage);
+		}
+	}
+}
