5929a6d354bbaf4e3517ef1bb29652c7dedf585b
trinhtiendat17 <datttps41395@gmail.com>
2024-07-03 13:41:25 +0700

    background

---
+++ Assets/Dat/Scripts/BackgroundController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private float startPos;
+    public GameObject cam;
+    public float parallaxEffect; 
+    void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    void Update()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
